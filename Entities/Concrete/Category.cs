273af25e9ef5499b1436a2f4cdc5a4f1e7ea5afe
mkilicarslan <mkilicarslankarapinar@gmail.com>
2022-09-07 15:09:56 +0300

    Add project files.

---
+++ Entities/Concrete/Category.cs
@@ -0,0 +1,16 @@
+﻿using Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Concrete
+{
+    public class Category:IEntity
+        //Çıplak Class Kalmasın - Herhangi bir class bir kalıtım veya interface almıyorsa
+        //ileride sorun çıkacaktır.
+    {
+        public int CategoryId { get; set; }
+        public string CategotyName { get; set; }
+
+    }
+}
